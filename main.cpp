cd87c30657d7340f76e572f1b2f5bb53c2d43976
Hyunmin Jung <jung@hyunmin.dev>
2020-12-23 12:47:46 +0900

    Make Prototype Code

--- main.cpp
+++ main.cpp
@@ -1,7 +1,22 @@
 #include <iostream>
+#include <vector>
 
+
+using namespace std;
+
+int operand;
+int result;
+
+int solution(int _operand, int _result) {
+  operand = _operand;
+  result = _result;
+
+  int answer = 0;
+
+  return answer;
+}
 
 int main() {
-  std::cout << "Hello, World!" << std::endl;
+  cout << solution(5,12) << endl;
   return 0;
 }
